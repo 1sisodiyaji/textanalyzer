c83ea90790236edcf20c0b6d7f5aaf78b2335ced
1sisodiyaji <93850014+1sisodiyaji@users.noreply.github.com>
2024-01-15 20:25:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import './index.css';
 import NavBar from "./component/NavBar";
 import TextForm from "./component/Textarea";
+import React , {useState} from 'react';
 
 function App() {
+  const [mode ,setMode] = useState('light');
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#131313';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <div className="bg-dark vh-100">
-    <NavBar/>
-    <TextForm/>
+    <NavBar mode = {mode} toggleMode = {toggleMode}/>
+    <TextForm mode = {mode}/>
     </div>
   );
 }
